4dbf1540a51a0391a33bd58632b1326ca997a3a3
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-22 17:27:16 +0430

    endpoints update

--- server.js
+++ server.js
@@ -1,14 +1,56 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id : '123',
+            name : 'john',
+            email : 'john@gmail.com',
+            password : '1234',
+            entries : 0,
+            joined : new Date()
+        },
+        {
+            id : '124',
+            name : 'ali',
+            email : 'ali@gmail.com',
+            password : '1234',
+            entries : 0,
+            joined : new Date()
+        }
+    ]
+};
 
 app.get('/', (req, res) => {
     res.send('this is working');
 });
 
 app.post('/signin', (req, res) => {
-    res.send('signin');
+    if(req.body.email === database.users[0].email && 
+       req.body.password === database.users[0].password ){
+        res.json('success');
+    }else{
+        res.status(400).json('error logging in!');
+    }
 });
+
+app.post('/register', (req, res) => {
+    const { name, email, password} = req.body;
+    database.users.push({
+        id : '123',
+        name : name,
+        email : email,
+        password : password,
+        entries : 0,
+        joined : new Date()
+    })
+    res.json(database.users[database.users.length - 1]);
+});
+
+
 
 app.listen(3000, () => {
     console.log('app is running');
