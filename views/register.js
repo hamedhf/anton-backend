649be2ccf0f1aab16ee97ef491346d242e60afed
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-25 02:20:34 +0430

    register api update

--- views/register.js
+++ views/register.js
@@ -1,30 +1,21 @@
 import { response } from "express";
 
 const register = (props) => {
-    const {database} = props;
+	const {database} = props;
 
-    return (req, res) => {
-        const { name, email, password} = req.body;
+	return (req, res) => {
+		const { name, email, password} = req.body;
 
-        database('users')
-            .returning('*')
-            .insert({
-                name, 
-                email, 
-                joined: new Date()
-            })
-            .then(user => res.json(user[0]))
-            .catch(err => res.status(400).json('unable to register'));
-
-        // database.users.push({
-        //     id : '211',
-        //     name : name,
-        //     email : email,
-        //     password : password,
-        //     entries : 0,
-        //     joined : new Date()
-        // })
-    };
+		database('users')
+			.returning('*')
+			.insert({
+				name, 
+				email, 
+				joined: new Date()
+			})
+			.then(user => res.json(user[0]))
+			.catch(err => res.status(400).json('unable to register'));
+	};
 };
 
 export default register;
